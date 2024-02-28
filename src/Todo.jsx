cf19f79b96aec39d08b576c3f40de5c93b7d6f5d
KJR020 <johnjiro1114@gmail.com>
2024-02-29 06:31:43 +0900

    Fix: タグを追加 todo-itemとlist-row

--- src/Todo.jsx
+++ src/Todo.jsx
@@ -1,37 +1,48 @@
+import { useState } from "react";
 import "./styles.css";
 
 export default function Todo() {
+  const [incompleteTodos, setIncompleteTodos] = useState(["TODOです1", "TODOです2"])
+
   return (
     <>
-      <div>
+      <div className="input-area">
         <input placeholder="TODOを入力" />
         <button>追加</button>
       </div>
-      <div>
-        <p>未完了のTODO</p>
+      <div className="incomplete-area">
+        <p className="title">未完了のTODO</p>
         <ul>
           <li>
-            <p>todoです</p>
-            <button>完了</button>
-            <button>削除</button>
+            <div className="list-row">
+              <p className="todo-item">todoです</p>
+              <button>完了</button>
+              <button>削除</button>
+            </div>
           </li>
           <li>
-            <p>todoです</p>
-            <button>完了</button>
-            <button>削除</button>
+            <div>
+              <p className="todo-item">todoです</p>
+              <button>完了</button>
+              <button>削除</button>
+            </div>
           </li>
         </ul>
       </div>
-      <div>
-        <p>完了のTODO</p>
+      <div className="complete-area">
+        <p className="title">完了のTODO</p>
         <ul>
           <li>
-            <p>TODOでした</p>
+            <div className="list-row">
+            <p className="todo-item">TODOでした</p>
             <button>戻す</button>
+            </div>
           </li>
           <li>
-            <p>TODOでした</p>
-            <button>戻す</button>
+            <div className="list-row">
+              <p className="todo-item">TODOでした</p>
+              <button>戻す</button>
+            </div>
           </li>
         </ul>
       </div>
